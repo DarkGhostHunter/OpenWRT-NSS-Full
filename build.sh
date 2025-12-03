4255c7c9bdc4555f4e99d0a1dc7e930a93826948
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2025-12-02 21:43:33 -0300

    Ensure bridger is never made

--- build.sh
+++ build.sh
@@ -562,6 +562,10 @@ add_package "luci-app-plexmediaserver"
 add_package "luci-app-tailscale"
 add_package "luci-app-zerotier"
 
+# Ensure bridger (for Mediatek) is not included
+sed -i '/CONFIG_PACKAGE_bridger/d' .config
+echo "# CONFIG_PACKAGE_bridger is not set" >> .config
+
 # Force Avahi No-DBus variant to fix permission crashes
 sed -i '/CONFIG_PACKAGE_avahi-dbus-daemon/d' .config
 echo "# CONFIG_PACKAGE_avahi-dbus-daemon is not set" >> .config
